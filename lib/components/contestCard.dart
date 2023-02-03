aaff14b44ffbc06aabaa34359035e7aaa00d40f2
Jayesh Patil <jp7470484@gmail.com>
2023-02-03 23:33:55 +0530

    final commit hai

---
+++ lib/components/contestCard.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../color_helper/defaultColor.dart';
+import '../font_helper/default_fonts.dart';
+
+class ContestCard extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+              margin: EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  Icon(Icons.watch_later_outlined,
+                      size: 16),
+                  Container(
+                      margin: EdgeInsets.only(left: 4),
+                      child: Text("Closing in 1 hour", style: textStyleMinDesc())),
+                ],
+              )),
+          Container(
+            margin: EdgeInsets.only(bottom: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ImageIcon(AssetImage('assets/imgs/contest_icon.png'), size: 60,),
+                Container(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Icon(Icons.emoji_events, color: starColor(),),
+                          Text('10,000', style: textStylePercent(),),
+                        ],
+                      ),
+                      Container(
+                        margin: EdgeInsets.only(top: 10),
+                        decoration: BoxDecoration(
+                            color: Colors.green,
+                            borderRadius: BorderRadius.circular(14)
+                        ),
+                        child: Center(
+                            child: Padding(
+                          padding: const EdgeInsets.only(top: 8, bottom: 8, left: 20, right: 20),
+                          child: Row(
+                            children: [
+                              Text('Join ', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+                              Text('10', style: TextStyle(color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),),
+                              Icon(Icons.currency_rupee, color: starColor(),),
+                            ],
+                          ),
+                        )),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          new Divider(
+            thickness: 2,
+            color: defaultBgColor()
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Starts: 1 feb | 10:00 am", style: textStyleMinDesc()),
+                Text("Ends: 2 feb | 10:00 am", style: textStyleMinDesc())
+              ],
+            )
+          )
+        ],
+      ),
+    );
+  }
+
+}
--- lib/pages/my_games.dart
+++ lib/pages/my_games.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../color_helper/defaultColor.dart';
+import '../components/contestCard.dart';
 import '../components/queCard.dart';
 
 class MyGamePage extends StatelessWidget{
@@ -68,7 +69,7 @@ class predictTab extends StatelessWidget{
           child: SingleChildScrollView(
             child: Column(
               children: [
-                TabSection(),
+                TabSectionPre(),
                 Container(
                   margin: EdgeInsets.only(bottom: 8, left: 8, right: 8),
                   child: Column(
@@ -140,23 +141,95 @@ class predictTab extends StatelessWidget{
 }
 
 class contestTab extends StatelessWidget{
+  static int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: Center(child: Text('History of Users')),
+      body: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              TabSectionCon(),
+              Container(
+                margin: EdgeInsets.only(bottom: 8, left: 8, right: 8),
+                child: Column(
+                  children: [
+                    Container(
+                        margin: EdgeInsets.only(top: 8),
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          elevation: MyGamePage.elevationValue,
+                          child: ContestCard(),
+                        )
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(top: 8),
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          elevation: MyGamePage.elevationValue,
+                          child: ContestCard(),
+                        )
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(top: 8),
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          elevation: MyGamePage.elevationValue,
+                          child: ContestCard(),
+                        )
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(top: 8),
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          elevation: MyGamePage.elevationValue,
+                          child: ContestCard(),
+                        )
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(top: 8),
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          elevation: MyGamePage.elevationValue,
+                          child: ContestCard(),
+                        )
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
-class TabSection extends StatefulWidget{
+class TabSectionPre extends StatefulWidget{
   @override
   State<StatefulWidget> createState() {
-    return TabSectionState();
+    return TabSectionPreState();
   }
 }
 
-class TabSectionState extends State<TabSection>{
+class TabSectionPreState extends State<TabSectionPre>{
 
   List<String> items = [
     'Available',
@@ -191,6 +264,56 @@ class TabSectionState extends State<TabSection>{
                     child: Text(items[index], style: TextStyle(color: predictTab.currentIndex == index ? Colors.white : Colors.black),),
                   )),
                 ),
+            );
+          }
+      ),
+    );
+  }
+
+}
+
+class TabSectionCon extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return TabSectionConState();
+  }
+}
+
+class TabSectionConState extends State<TabSectionCon>{
+
+  List<String> items = [
+    'Available',
+    'Live',
+    'Completed',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      child: ListView.builder(
+          physics: const BouncingScrollPhysics(),
+          scrollDirection: Axis.horizontal,
+          padding: const EdgeInsets.only(top: 10, left: 10, right: 10),
+          itemCount: items.length,
+          itemBuilder: (BuildContext context, int index) {
+            return GestureDetector(
+              onTap: (){
+                setState((){
+                  contestTab.currentIndex = index;
+                });
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                    color: contestTab.currentIndex == index ? Colors.black : Colors.white,
+                    borderRadius: BorderRadius.circular(14)
+                ),
+                margin: EdgeInsets.only(right: 10),
+                child: Center(child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10),
+                  child: Text(items[index], style: TextStyle(color: contestTab.currentIndex == index ? Colors.white : Colors.black),),
+                )),
+              ),
             );
           }
       ),
