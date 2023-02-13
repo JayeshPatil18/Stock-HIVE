ddb2f8168be04ef7d752f50d8c175a1d43e40d19
Jayesh Patil <jp7470484@gmail.com>
2023-02-13 12:04:29 +0530

    final commit yes

--- lib/auth_pages/welcome_page.dart
+++ lib/auth_pages/welcome_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:stock_prediction/color_helper/defaultColor.dart';
 import 'package:stock_prediction/font_helper/default_fonts.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 
 class WelcomePage extends StatefulWidget {
   @override
@@ -14,42 +16,40 @@ class _WelcomePageState extends State<WelcomePage> {
 
   int currentIndex = 0;
 
-  List lottieAssets = [
-    Lottie.asset('assets/anim/predict_img.json',
-        height: 260, reverse: false, repeat: true, fit: BoxFit.cover),
-    Lottie.asset('assets/anim/contest_img.json',
-        height: 260, reverse: false, repeat: true, fit: BoxFit.cover),
-    Lottie.asset('assets/anim/discuss_img.json',
-        height: 200, reverse: false, repeat: true, fit: BoxFit.cover)
-  ];
-
   List sections = [
-    Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    Stack(
       children: [
-        Lottie.asset('assets/anim/predict_img.json',
-            height: 260, reverse: false, repeat: true, fit: BoxFit.cover),
         Container(
-            margin: EdgeInsets.only(top: 20),
+          margin: EdgeInsets.only(bottom: 60),
+          alignment: Alignment.center,
+          child: Lottie.asset('assets/anim/predict_img.json',
+              height: 300, reverse: false, repeat: true, fit: BoxFit.cover),
+        ),
+        Align(
+            alignment: AlignmentDirectional.bottomCenter, // <-- SEE HERE
             child: Text('Predict & Win', style: textStyleDefault())),
-      ],
-    ),
-    Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Lottie.asset('assets/anim/contest_img.json',
-            height: 260, reverse: false, repeat: true, fit: BoxFit.cover),
-        Container(
-            margin: EdgeInsets.only(top: 20),
-            child: Text('Play Contest', style: textStyleDefault())),
       ],
     ),
     Stack(
       children: [
         Container(
+          margin: EdgeInsets.only(bottom: 40),
+          alignment: Alignment.center,
+          child: Lottie.asset('assets/anim/contest_img.json',
+              height: 280, reverse: false, repeat: true, fit: BoxFit.cover),
+        ),
+        Align(
+            alignment: AlignmentDirectional.bottomCenter, // <-- SEE HERE
+            child: Text('Join Contests', style: textStyleDefault())),
+      ],
+    ),
+    Stack(
+      children: [
+        Container(
+          margin: EdgeInsets.only(bottom: 20),
           alignment: Alignment.center,
           child: Lottie.asset('assets/anim/discuss_img.json',
-              height: 220, reverse: false, repeat: true, fit: BoxFit.cover),
+              height: 300, reverse: false, repeat: true, fit: BoxFit.cover),
         ),
         Align(
             alignment: AlignmentDirectional.bottomCenter, // <-- SEE HERE
@@ -64,40 +64,59 @@ class _WelcomePageState extends State<WelcomePage> {
         body: Column(
           children: [
         Container(
-          height: 320,
-          margin: EdgeInsets.only(top: 60, bottom: 40),
-          child: PageView.builder(
-            onPageChanged: (int index){
-              setState(() {
-                currentIndex = index;
-              });
-            },
-              itemCount: 3,
-              itemBuilder: (context, index) {
-                return sections[index];
-              }),
-        ),
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(
-                  sections.length,
-                      (index) => buildDot(index, context),
+          height: 340,
+          margin: EdgeInsets.only(top: 40, bottom: 40),
+          child: ListView(
+            children: [
+              CarouselSlider(
+                items: [
+
+                  //1st Image of Slider
+                  sections[0],
+
+                  //2nd Image of Slider
+                  sections[1],
+
+                  //3rd Image of Slider
+                  sections[2],
+
+                ],
+
+                //Slider Container properties
+                options: CarouselOptions(
+                  height: 300.0,
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 16 / 9,
+                  autoPlayCurve: Curves.fastOutSlowIn,
+                  enableInfiniteScroll: true,
+                  autoPlayAnimationDuration: Duration(milliseconds: 800),
+                  viewportFraction: 0.8,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      currentIndex = index;
+                    });
+                  },
                 ),
               ),
+            ],
+          )
+        ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: sections.map((section) {
+                return Container(
+                  width: 10,
+                  height: 10,
+                  margin: EdgeInsets.symmetric(horizontal: 4),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: currentIndex == sections.indexOf(section) ? Colors.black : Color(0xffeaeaea)
+                  ),
+                );
+              }).toList(),
             ),
       ],
     ));
-  }
-  Container buildDot(int index, BuildContext context) {
-    return Container(
-      height: 10,
-      width: currentIndex == index ? 25 : 10,
-      margin: EdgeInsets.only(right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(20),
-        color: Colors.black,
-      ),
-    );
   }
 }
