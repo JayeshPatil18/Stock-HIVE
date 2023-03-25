3d59e5a6b6308f8bfa6df4efea5053fa910974b7
Jayesh Patil <jp7470484@gmail.com>
2023-03-25 10:08:40 +0530

    like commit final

--- lib/pages/discussion.dart
+++ lib/pages/discussion.dart
@@ -27,6 +27,14 @@ class _DiscussionPageState extends State<DiscussionPage>{
 
   List<TweetsModel> tweetsList = [];
 
+  Future _refresh() async{
+    var list = await getTweets();
+    setState(() {
+      tweetsList.clear();
+      tweetsList = list;
+    });
+  }
+
   @override
   void initState() {
   }
@@ -36,36 +44,42 @@ class _DiscussionPageState extends State<DiscussionPage>{
 
     return Scaffold(
       backgroundColor: Colors.transparent,
-      body: FutureBuilder(
-        future: getTweets(),
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          if (snapshot.data == null) {
-            return Center(child: Text('Loading...'),);
-          } else {
-            return ListView.builder(
-              padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
-              itemCount: tweetsList.length,
-              itemBuilder: (context, index) {
-                var tweet = tweetsList[index];
-                return GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => TweetPage(tId: tweet.tId.toString(), tweetsModel: tweet,)));
-                  },
-                  child: Container(
-                      margin: EdgeInsets.only(top: 8),
-                      width: double.infinity,
-                      child: Card(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        elevation: elevationValue,
-                        child: DiscussModel(tId: tweet.tId.toString(), fullname: tweet.fullname.toString(), username: tweet.username.toString(), tTxt: tweet.tTxt.toString(), tDate: tweet.tDateTime.toString(), tLikes: tweet.tLikes.toString(), tComments: tweet.tComments.toString(), tUrl: tweet.tUrl.toString(), isLiked: tweet.isLiked.toString(),),
-                      )),
-                );
-              }
-            );
-          }
-        },
+      body: RefreshIndicator(
+        onRefresh: _refresh,
+        child: FutureBuilder(
+          future: getTweets(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.data == null) {
+              return Center(child: Text('Loading...'),);
+            } else {
+              return ListView.builder(
+                padding: EdgeInsets.only(bottom: 10, left: 10, right: 10),
+                itemCount: tweetsList.length,
+                itemBuilder: (context, index) {
+                  var tweet = tweetsList[index];
+                  return GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => TweetPage(tId: tweet.tId.toString(), tweetsModel: tweet,)))
+                      .then((value) => setState(() {
+                        _refresh();
+                      }));
+                    },
+                    child: Container(
+                        margin: EdgeInsets.only(top: 8),
+                        width: double.infinity,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          elevation: elevationValue,
+                          child: DiscussModel(tId: tweet.tId.toString(), fullname: tweet.fullname.toString(), username: tweet.username.toString(), tTxt: tweet.tTxt.toString(), tDate: tweet.tDateTime.toString(), tLikes: tweet.tLikes.toString(), tComments: tweet.tComments.toString(), tUrl: tweet.tUrl.toString(), isLiked: tweet.isLiked.toString(),),
+                        )),
+                  );
+                }
+              );
+            }
+          },
+        ),
       ),
     );
   }
