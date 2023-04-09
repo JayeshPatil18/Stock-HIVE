780d0e267687ceb91c40be558e325dc2d4fecd54
Jayesh Patil <jp7470484@gmail.com>
2023-04-09 11:52:58 +0530

    final commit

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,5 +1,8 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
+
 import '../auth_pages/phoneno.dart';
 import '../data_models/UserModel.dart';
 import 'package:firebase_storage/firebase_storage.dart%20';
@@ -265,7 +268,7 @@ class EditProfileState extends State<EditProfile> {
 
                               try {
                                 await _uploadFile(_imageFile!.path);
-                              } catch(e){}
+                              } catch (e) {}
 
                               bool isUpdated = await updateProfile();
 
@@ -274,7 +277,7 @@ class EditProfileState extends State<EditProfile> {
 
                                 FocusManager.instance.primaryFocus?.unfocus();
                                 Future.delayed(
-                                    const Duration(milliseconds: 500), () {
+                                    const Duration(seconds: 1), () {
                                   Navigator.pop(context);
                                 });
                               } else {
@@ -341,6 +344,8 @@ class EditProfileState extends State<EditProfile> {
 }
 
 class ChangePhoneNum extends StatefulWidget {
+  static String verify = "";
+
   @override
   State<StatefulWidget> createState() {
     return ChangePhoneNumState();
@@ -350,6 +355,8 @@ class ChangePhoneNum extends StatefulWidget {
 class ChangePhoneNumState extends State<ChangePhoneNum> {
   String _buttonText = 'Change';
   var boarderWidth = 1.4;
+
+  String phoneNo = "";
 
   final phoneNoController = TextEditingController();
 
@@ -374,27 +381,23 @@ class ChangePhoneNumState extends State<ChangePhoneNum> {
             children: [
               Form(
                 key: _myFormKey,
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
+                child: IntlPhoneField(
                   controller: phoneNoController,
-                  validator: (String? msg) {
-                msg = msg?.trim();
-                if (msg!.isEmpty) {
-                  return "Please enter Phone no.";
-                } else {
-                  return null;
-                }
-              },
                   decoration: InputDecoration(
-                      labelText: 'Phone number',
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(13),
-                          borderSide: BorderSide(
-                              color: Colors.black, width: boarderWidth)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(13),
-                          borderSide: BorderSide(
-                              color: Colors.black, width: boarderWidth))),
+                    labelText: 'Phone Number',
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(13),
+                        borderSide: BorderSide(
+                            color: Colors.black, width: boarderWidth)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(13),
+                        borderSide: BorderSide(
+                            color: Colors.black, width: boarderWidth)),
+                  ),
+                  initialCountryCode: 'IN',
+                  onChanged: (phone) {
+                    phoneNo = phone.completeNumber;
+                  },
                 ),
               ),
               SizedBox(
@@ -409,32 +412,48 @@ class ChangePhoneNumState extends State<ChangePhoneNum> {
                           backgroundColor: Colors.black,
                           shape: RoundedRectangleBorder(
                               borderRadius: BorderRadius.circular(13))),
-                      onPressed: () async{
+                      onPressed: () async {
                         if (!isClicked) {
-                      isClicked = true;
+                          isClicked = true;
 
-                      bool isValid = _myFormKey.currentState!.validate();
+                          bool isValid = _myFormKey.currentState!.validate();
 
-                      if (isValid) {
-                        _setButtonText("Loading...");
+                          if (isValid) {
+                            _setButtonText("Loading...");
 
-                        bool isDone = await isPhoneNoUpdated(logusername, phoneNoController.text);
+                            // Verification Code sending
+                            await FirebaseAuth.instance.verifyPhoneNumber(
+                              phoneNumber: phoneNo,
+                              verificationCompleted:
+                                  (PhoneAuthCredential credential) {},
+                              verificationFailed: (FirebaseAuthException e) {},
+                              codeSent:
+                                  (String verificationId, int? resendToken) {
+                                ChangePhoneNum.verify = verificationId;
+                              },
+                              codeAutoRetrievalTimeout:
+                                  (String verificationId) {},
+                            );
 
-                        if (isDone) {
-                          _setButtonText("Changed!");
-                          Future.delayed(const Duration(seconds: 1), () {
-                            Navigator.pop(context);
-                          });
-                        } else {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                              const SnackBar(
-                                  content: Text('Something Went Wrong.')));
-                          _setButtonText("Change");
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content:
+                                    Text('Verification Code has been sent.')));
+
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => VerifyPhoneNoChange(
+                                        phoneNo: phoneNo))).then((value) => {
+                                  FocusManager.instance.primaryFocus?.unfocus(),
+                                  Future.delayed(
+                                      const Duration(milliseconds: 500), () {
+                                    Navigator.pop(context);
+                                  })
+                                });
+                          }
+
+                          isClicked = false;
                         }
-                      }
-
-                      isClicked = false;
-                    }
                       },
                       child: Text(
                         _buttonText,
@@ -448,25 +467,6 @@ class ChangePhoneNumState extends State<ChangePhoneNum> {
         ),
       ),
     );
-  }
-
-  Future<bool> isPhoneNoUpdated(String username, String phoneNo) async {
-    try {
-      final url = Uri.parse('$globalApiUrl/users/edit/phoneno');
-      final headers = {'Content-Type': 'application/json'};
-      final body = json.encode({
-        'username': username,
-        'phoneNo': phoneNo,
-      });
-      final response = await http.post(url, headers: headers, body: body);
-      if (response.statusCode == 200) {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (e) {
-      return false;
-    }
   }
 }
 
@@ -715,6 +715,217 @@ class TermsConditionState extends State<TermsCondition> {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class VerifyPhoneNoChange extends StatefulWidget {
+  String phoneNo;
+
+  VerifyPhoneNoChange({
+    required this.phoneNo,
+  });
+
+  @override
+  State<StatefulWidget> createState() {
+    return VerifyPhoneNoChangeState(phoneNo);
+  }
+}
+
+class VerifyPhoneNoChangeState extends State<VerifyPhoneNoChange> {
+  String phoneNo = "";
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  final TextEditingController _fieldOne = TextEditingController();
+  final TextEditingController _fieldTwo = TextEditingController();
+  final TextEditingController _fieldThree = TextEditingController();
+  final TextEditingController _fieldFour = TextEditingController();
+  final TextEditingController _fieldFive = TextEditingController();
+  final TextEditingController _fieldSix = TextEditingController();
+
+  String finalCode = "";
+
+  var boarderWidth = 1.4;
+
+  VerifyPhoneNoChangeState(String phoneNo) {
+    this.phoneNo = phoneNo;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.only(top: 80, bottom: 40, left: 20, right: 20),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  margin: EdgeInsets.only(top: 20, bottom: 30),
+                  child: Text('Verify Phone number', style: textBig())),
+              Container(
+                padding: EdgeInsets.only(top: 20, bottom: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    OtpInput(_fieldOne, true),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    OtpInput(_fieldTwo, false),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    OtpInput(_fieldThree, false),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    OtpInput(_fieldFour, false),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    OtpInput(_fieldFive, false),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    OtpInput(_fieldSix, false)
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 40,
+              ),
+              Container(
+                  padding: EdgeInsets.only(left: 40, right: 40),
+                  child: Text(
+                    "Verification Code has been send to ${phoneNo}",
+                    style: TextStyle(fontSize: 16),
+                    textAlign: TextAlign.center,
+                  )),
+              SizedBox(
+                height: 60,
+              ),
+              Container(
+                  height: 50,
+                  width: double.infinity,
+                  margin: EdgeInsets.only(bottom: 60, left: 20, right: 20),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(13))),
+                      onPressed: () async {
+                        finalCode = _fieldOne.text +
+                            _fieldTwo.text +
+                            _fieldThree.text +
+                            _fieldFour.text +
+                            _fieldFive.text +
+                            _fieldSix.text;
+
+                        try {
+                          // PhoneAuthCredential credential = PhoneAuthProvider.credential(verificationId: SignUp.verify, smsCode: finalCode);
+
+                          // await auth.signInWithCredential(credential);
+
+                          bool isDone =
+                              await isPhoneNoUpdated(logusername, phoneNo);
+
+                          Navigator.pop(context);
+                        } catch (e) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                              content: Text('Invalid Verification Code')));
+                        }
+                      },
+                      child: Text(
+                        'Verify',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 18),
+                      ))),
+              Text("Didn’t Received the Code?"),
+              Container(
+                height: 4,
+              ),
+              InkWell(
+                onTap: () async {
+                  // Verification Code sending
+                  await FirebaseAuth.instance.verifyPhoneNumber(
+                    phoneNumber: phoneNo,
+                    verificationCompleted: (PhoneAuthCredential credential) {},
+                    verificationFailed: (FirebaseAuthException e) {},
+                    codeSent: (String verificationId, int? resendToken) {
+                      ChangePhoneNum.verify = verificationId;
+                    },
+                    codeAutoRetrievalTimeout: (String verificationId) {},
+                  );
+
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                      content: Text('Verification Code has been sent.')));
+                },
+                child: Text(
+                  "Resend",
+                  style: textStyleLeader(),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<bool> isPhoneNoUpdated(String username, String phoneNo) async {
+    try {
+      final url = Uri.parse('$globalApiUrl/users/edit/phoneno');
+      final headers = {'Content-Type': 'application/json'};
+      final body = json.encode({
+        'username': username,
+        'phoneNo': phoneNo,
+      });
+      final response = await http.post(url, headers: headers, body: body);
+      if (response.statusCode == 200) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      return false;
+    }
+  }
+}
+
+class OtpInput extends StatelessWidget {
+  final TextEditingController controller;
+  final bool autoFocus;
+
+  const OtpInput(this.controller, this.autoFocus, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      width: 50,
+      child: TextField(
+        autofocus: autoFocus,
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+        controller: controller,
+        maxLength: 1,
+        cursorColor: Theme.of(context).primaryColor,
+        decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            counterText: '',
+            hintStyle: TextStyle(color: Colors.black, fontSize: 20.0)),
+        onChanged: (value) {
+          if (value.length == 1) {
+            FocusScope.of(context).nextFocus();
+          }
+        },
       ),
     );
   }
